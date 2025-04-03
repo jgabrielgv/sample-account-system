6a152d24af71335cdc53c79da3b7e3aafb1c413d
Juan Gomez <jgmezvargas@gmail.com>
2025-04-03 10:20:39 -0600

    email: send email

--- AccountFacade.java
+++ AccountFacade.java
@@ -1,12 +1,15 @@
 public class AccountFacade {
 
     private final IAccountService accountService;
+    private final IEmailService emailService;
     
-        public AccountFacade(IAccountService accountService) {
+        public AccountFacade(IAccountService accountService, IEmailService emailService) {
             this.accountService = accountService;
-    }
+            this.emailService = emailService;
+        }
 
     public void changePassword(String userId, String newPassword) {
         accountService.changePassword(userId, newPassword);
+        emailService.email(userId, "Cambio de clave", "Importante: cambio de clave realizado");
     }
 }
--- Main.java
+++ Main.java
@@ -1,8 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         AccountService accountService = new AccountService();
+        EmailService emailService = new EmailService();
 
-        AccountFacade facade = new AccountFacade(accountService);
+        AccountFacade facade = new AccountFacade(accountService, emailService);
 
         facade.changePassword("testuserid", "newfield");
     }
